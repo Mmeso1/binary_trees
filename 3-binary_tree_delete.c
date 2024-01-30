953c00213444adb932799096fa7bdbf4c9ea2453
Mmeso1 <anisiubammesoma8@gmail.com>
2024-01-30 08:26:03 -0800

    Betty fix for task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,8 +1,7 @@
 #include "binary_trees.h"
 /**
- *
  * binary_tree_delete -  deletes an entire binary tree
- * tree: a pointer to the root node of the tree to delete
+ * @tree: a pointer to the root node of the tree to delete
  * Return: nothing
  */
 void binary_tree_delete(binary_tree_t *tree)
