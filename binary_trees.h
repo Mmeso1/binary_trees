ef1ee3200389def7274913193c89e33384a4c17e
Mmeso1 <anisiubammesoma8@gmail.com>
2024-01-31 08:44:33 -0800

    Advanced task 19 done

--- binary_trees.h
+++ binary_trees.h
@@ -27,6 +27,7 @@ int binary_tree_is_root(const binary_tree_t *node);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_print(const binary_tree_t *);
 void binary_tree_delete(binary_tree_t *tree);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
