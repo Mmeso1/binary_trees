2a3b6668dc0bce7f7768db5107decadb92aa6a7a
Mmeso1 <anisiubammesoma8@gmail.com>
2024-01-31 09:38:25 -0800

    Task 20, correction checker error

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -29,7 +29,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
  */
 void process_level(const binary_tree_t *tree, void (*func)(int), int level)
 {
-	if (tree == NULL)
+	if (tree == NULL || func == NULL)
 		return;
 
 	if (level == 1)
