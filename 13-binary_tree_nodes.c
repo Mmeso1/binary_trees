5534abbbe4ca1491f42a55315b46e3b12cb1ad27
Mmeso1 <anisiubammesoma8@gmail.com>
2024-02-01 00:01:13 -0800

    Task 13 and it's alternative

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,16 +7,17 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t left, right;
+	size_t left, right, count = 0;
 
 	if (!tree)
 		return (0);
 
 	if (tree->left || tree->right)
 	{
-		left = binary_tree_nodes(tree->left);
-		right = binary_tree_nodes(tree->right);
-		return (1 + left + right);
+		count += 1;
 	}
-	return (0);
+
+	left = binary_tree_nodes(tree->left);
+	right = binary_tree_nodes(tree->right);
+	return (count + left + right);
 }
