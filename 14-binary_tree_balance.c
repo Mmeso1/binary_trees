17ca4c91eac15c3a28cee800a5a589ffdf60eeb2
Mmeso1 <anisiubammesoma8@gmail.com>
2024-02-01 01:30:48 -0800

    Task 14 betty error fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,8 +14,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0; 
-	r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0; 
+	l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 	return (l_height - r_height);
 }
