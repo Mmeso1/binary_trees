88ae7f910e6e0f267b607ba6563cda613a6f6e02
Mmeso1 <anisiubammesoma8@gmail.com>
2024-02-01 03:25:17 -0800

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -21,9 +21,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (binary_tree_nodes(tree) != (size_t)(2 ^ l_height))
 		return (0);
 
-	binary_tree_is_perfect(tree->left);
-	binary_tree_is_perfect(tree->right);
-
 	return (1);
 }
 
