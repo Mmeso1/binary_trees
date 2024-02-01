2714b9cc842e3b239a6025d6f4334b4be5835df6
Mmeso1 <anisiubammesoma8@gmail.com>
2024-01-31 21:31:23 -0800

    Betty fix for advanced task 23

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_right: performs a right-rotation on a binary tree
+ * binary_tree_rotate_right - performs a right-rotation on a binary tree
  * @tree: a pointer to the root node of the tree to rotate
  * Return: a pointer to the new root node of the tree once rotated
  */
